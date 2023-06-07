d50fc7283847df74ffb17ac3d994db90cd9874c9
Yusufly <123965338+Yusufly@users.noreply.github.com>
2023-06-07 17:55:52 +0300

    ez bot

---
+++ question/question.js
@@ -0,0 +1,241 @@
+let MCQS = [{
+    question: "Ankara ne zaman başkent olmuştur?",
+    choice1: "1913",
+    choice2: "1923",
+    choice3: "1933",
+    choice4: "1943",
+    answer: 1
+},
+            {
+            question: "Hangisi bitki hücresinde bulunmayan bir organeldir?",
+    choice1: "Ribozom",
+    choice2: "Koful",
+    choice3: "Lizozom",
+    choice4: "Kloroplast",
+    answer: 2
+},
+            {
+            question: "Hangisi asal bir sayıdır?",
+    choice1: "4",
+    choice2: "7",
+    choice3: "9",
+    choice4: "15",
+    answer: 1
+},
+            {
+            question: "Hangi gezegen güneşe daha uzak mesafededir?",
+    choice1: "Neptün",
+    choice2: "Uranüs",
+    choice3: "Mars",
+    choice4: "Jüpiter",
+    answer: 0
+},
+            {
+            question: "İzafiyet teorisi kime aittir?",
+    choice1: "Isaac Newton",
+    choice2: "Albert Einstein",
+    choice3: "Galileo Galilei",
+    choice4: "Marie Curie",
+    answer: 1
+},
+            {
+            question: "Samanyolu Galaksisine en yakın galaksi hangisidir?",
+    choice1: "Galaksi NGC 6753",
+    choice2: "Alcyoneus",
+    choice3: "Andromeda",
+    choice4: "Büyük Macellan Bulutu",
+    answer: 2
+},
+            {
+               question: "Simgesi (Ca) olan elementin adı nedir?",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "Kalsiyum",
+    answer: 3
+},
+{
+    question: "Dünyanın ilk kadın savaş pilotu kimdir?",
+choice1: "",
+choice2: "Sabiha Gökçen",
+choice3: "",
+choice4: "",
+answer: 1
+},
+{
+    question: "Türkiyenin hapis cezası alan ilk bilgisayar korsanı kimdir?",
+choice1: "Tamer Şahin",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "Her yüz yılda 7 cm yere yaklaşan pisa kulesi hangi yöne doğru eğilmektedir?",
+choice1: "Batı",
+choice2: "Doğu",
+choice3: "Kuzey",
+choice4: "Güney",
+answer: 3
+},
+{
+    question: "Yedi renkten oluşan gökkuşağının ortasındaki renk hangisidir?",
+choice1: "",
+choice2: "",
+choice3: "Yeşil",
+choice4: "",
+answer: 2
+},
+{
+    question: "Hollanda'nın başkenti neresidir?",
+choice1: "",
+choice2: "",
+choice3: "",
+choice4: "Amsterdam",
+answer: 3
+},
+{
+    question: "Türkiye'nin en büyük dağı hangisidir?",
+choice1: "Ağrı Dağı",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "Türkiye'nin en büyük gölü hangisidir?",
+choice1: "Van Gölü",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "Türk silahlı kuvvetlerinde hangi rütbe daha yüksektir?",
+choice1: "",
+choice2: "Korgeneral",
+choice3: "",
+choice4: "",
+answer: 1
+},
+{
+    question: "Kimler kepenek giyer?",
+choice1: "",
+choice2: "",
+choice3: "Çoban",
+choice4: "",
+answer: 2
+},
+{
+    question: "Yazları sıcak ve kurak; kışları ise soğuk ve karlıdır. Aşağıdakilerden hangisidir?",
+choice1: "Karasal İklim",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "Çocuk hakları günü hangi tarihte kutlanılmaktadır?",
+choice1: "",
+choice2: "",
+choice3: "",
+choice4: "20 Kasım",
+answer: 3
+},
+{
+    question: "Bir elektrik devresinde direnç hangi harfle gösterilir?",
+choice1: "",
+choice2: "",
+choice3: "",
+choice4: "R",
+answer: 3
+},
+{
+    question: "İstiklal Marşı hangi yıl yazılmıştır?",
+choice1: "",
+choice2: "",
+choice3: "1921",
+choice4: "",
+answer: 2
+},
+{
+    question: "Kuyucaklı Yusuf adlı eser kime aittir?",
+choice1: "",
+choice2: "",
+choice3: "Sabahattin Ali",
+choice4: "",
+answer: 2
+},
+{
+    question: "Hangisi ana renk değildir?",
+choice1: "Sarı",
+choice2: "Mavi",
+choice3: "Kırmızı",
+choice4: "Mor",
+answer: 3
+},
+{
+    question: "DNA onarımı hakkındaki bilimsel çalışmasıyla 2015 Nobel Kimya Ödülünü almaya hak kazanan Türk bilim insanı kimdir?",
+choice1: "Aziz Sancar",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "İtalya'da 14. Yüzyılın ikinci yarısında başlayan 'Yeniden Doğuş' anlamına gelen kavram hangisidir?",
+choice1: "Rönesans",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "Aya ilk defa kim ayak basan kişi kimdir?",
+choice1: "",
+choice2: "Neil Armstrong",
+choice3: "",
+choice4: "",
+answer: 1
+},
+{
+    question: "Dünyadaki suların yüzde kaçı tuzlu sudur?",
+choice1: "%93",
+choice2: "%37",
+choice3: "%97",
+choice4: "%75",
+answer: 2
+},
+{
+    question: "“Sinekli Bakkal” romanının yazarı aşağıdakilerden hangisidir?",
+choice1: "Halide Edip Adıvar",
+choice2: "",
+choice3: "",
+choice4: "",
+answer: 0
+},
+{
+    question: "Yazıyı icat exen ilk çağ uygarlığı hangisidir?",
+choice1: "",
+choice2: "",
+choice3: "",
+choice4: "Sümerler",
+answer: 3
+},
+{
+    question: "Mustafa Kemal Atatürk'ün nüfusa kayıtlı olduğu il hangisidir?",
+choice1: "",
+choice2: "",
+choice3: "",
+choice4: "Gaziantep",
+answer: 3
+},
+{
+    question: "Hangi ülkenin iki tane başkenti vardır",
+choice1: "",
+choice2: "",
+choice3: "Güney Afrika",
+choice4: "",
+answer: 2
+},
+]
